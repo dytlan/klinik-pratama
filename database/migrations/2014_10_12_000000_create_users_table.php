5d1f5c0d1321321ceab3692cc5d8f569892e3d2b
Dytlan <dytlanian@gmail.com>
2020-11-22 22:23:16 +0700

    feat: adding User Model and migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,9 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            // $table->timestamp('email_verified_at')->nullable();
+            $table->tinyInteger('role_id')->unsigned();
+            $table->bigInteger('media_id')->unsigned()->default(1);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
