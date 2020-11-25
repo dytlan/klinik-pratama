74c84463fbf71b40dfa181ef91b7469d82a4c600
dickaestu <dickaestusaputra@gmail.com>
2020-11-25 18:25:42 +0700

    WIP: Admin Dashboard

---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,26 @@
+<div class="main-sidebar sidebar-style-2">
+        <aside id="sidebar-wrapper">
+          <div class="sidebar-brand">
+            <a href="index.html"><span>ADMIN</span>
+            </a>
+          </div>
+          <ul class="sidebar-menu">
+            <li class="dropdown active">
+              <a href="index.html" class="nav-link"><i data-feather="monitor"></i><span>Dashboard</span></a>
+            </li>
+            <li class="dropdown">
+              <a href="index.html" class="nav-link"><i data-feather="users"></i><span>Kelola Users</span></a>
+            </li>
+            {{-- <li class="dropdown">
+              <a href="#" class="menu-toggle nav-link has-dropdown"><i
+                  data-feather="users"></i><span>Widgets</span></a>
+              <ul class="dropdown-menu">
+                <li><a class="nav-link" href="widget-chart.html">Chart Widgets</a></li>
+                <li><a class="nav-link" href="widget-data.html">Data Widgets</a></li>
+              </ul>
+            </li> --}}
+           
+         
+          </ul>
+        </aside>
+      </div>
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>@yield('title')</title>
+  @stack('prepend-style')
+  @include('includes.style')
+  @stack('addon-style')
+ 
+</head>
+
+<body>
+  <div class="loader"></div>
+  <div id="app">
+    <div class="main-wrapper main-wrapper-1">
+      <div class="navbar-bg"></div>
+      @include('includes.navbar')
+      @include('includes.sidebar')
+      <!-- Main Content -->
+      <div class="main-content">
+        @yield('content')
+      </div>
+      @include('includes.footer')
+    </div>
+  </div>
+  @stack('prepend-script')
+  @include('includes.script')
+  @stack('addon-script')
+
+</body>
+
+
+</html>
