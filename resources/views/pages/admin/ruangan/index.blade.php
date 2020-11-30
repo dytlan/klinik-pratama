bd25f3e3ca0477d30636385ac35c096594d43f98
dickaestu <dickaestusaputra@gmail.com>
2020-11-30 20:38:38 +0700

    WIP: Ruangan

--- resources/views/pages/admin/ruangan/index.blade.php
+++ resources/views/pages/admin/ruangan/index.blade.php
@@ -5,22 +5,18 @@
           <div class="section-body">
             <div class="row">
               <div class="col-12">
-                <a href="{{ route('user.create') }}" class="btn btn-primary btn-sm shadow-sm btn-icon icon-left mb-2"><i class="fas fa-plus"></i>Buat Akun</a>
+                <a href="{{ route('ruangan.create') }}" class="btn btn-primary btn-sm shadow-sm btn-icon icon-left mb-2"><i class="fas fa-plus"></i>Tambah Ruangan</a>
                 <div class="card">
                   <div class="card-header">
-                    <h4>Daftar Users</h4>
+                    <h4>Daftar Ruangan</h4>
                   </div>
                   <div class="card-body">
                     <div class="table-responsive">
-                      <table class="table table-striped table-hover" id="tableUsers" style="width:100%;">
+                      <table class="table table-striped table-hover" id="tableRuangan" style="width:100%;">
                         <thead>
                           <tr>
-                            <th>#</th>
-                            <th>Nama</th>
-                            <th>Username</th>
-                            <th>STR</th>
-                            <th>Masa berlaku</th>
-                            <th>Roles</th>
+                            <th>ID Ruangan</th>
+                            <th>Nama Ruangan</th>
                             <th>Action</th>
                           </tr>
                         </thead>
@@ -29,10 +25,6 @@
                         <tr>
                             <td>{{ $loop->iteration }}</td>
                             <td>{{ ucwords($user->nama) }}</td>
-                            <td>{{ $user->username }}</td>
-                            <td>{{ $user->str }}</td>
-                            <td>{{ \Carbon\Carbon::create($user->masa_berlaku)->format('d-m-Y') }}</td>
-                            <td>{{ ucwords($user->role->nama) }}</td>
                             <td>
                             <div class="d-flex pl-2">
                                 <a href="{{ route('user.edit',$user->id) }}" class="btn btn-warning btn-sm btn-icon mr-2">
@@ -69,7 +61,7 @@
   <script src="{{ asset('assets/bundles/datatables/DataTables-1.10.16/js/dataTables.bootstrap4.min.js') }}"></script>
 
   <script>
-    $('#tableUsers').DataTable({
+    $('#tableRuangan').DataTable({
        
     });
   </script>
